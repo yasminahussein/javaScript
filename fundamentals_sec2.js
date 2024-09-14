d1ed14769a23435de2fe5c57bf03b0184774a869
Yasmina Hussein <yasminaaahussien@gmail.com>
2024-09-14 20:54:29 +0300

    All 4 challenge_code_sec2 fundamentals_part2

--- fundamentals_sec2.js
+++ fundamentals_sec2.js
@@ -74,12 +74,12 @@ function calcTip(bill){
     }
     return tip ;
 }
-
+/*
 let bills = [100,350,200];
 let tips = [calcTip(bills[0]),calcTip(bills[1]) ,calcTip(bills[2])];
 let totals = [bills[0]+ tips[0] , bills[1]+ tips[1] , bills[2]+ tips[2]];
 console.log (bills ,tips ,totals);
-
+*/
 //Coding Challenge #3
 /*
 Let's go back to Mark and John comparing their BMIs! This time, let's use objects to 
@@ -118,3 +118,93 @@ let john ={
 };
 
 console.log(mark.calcBMI()>john.calcBMI()?`${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${john.fullName}'s (${john.calcBMI()})!`: `${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${mark.fullName}'s (${mark.calcBMI()})!`) ;
+
+
+//Coding Challenge #4
+/*
+Let's improve Steven's tip calculator even more, this time using loops!
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate 
+tips and total values (bill + tip) for every bill value in the bills array. Use a for
+loop to perform the 10 calculations!
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the 
+tips and totals arrays �
+Bonus:
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as 
+an argument. This function calculates the average of all numbers in the given 
+array. This is a difficult challenge (we haven't done this before)! Here is how to 
+solve it:
+4.1. First, you will need to add up all values in the array. To do the addition, 
+start by creating a variable 'sum' that starts at 0. Then loop over the 
+array using a for loop. In each iteration, add the current value to the 
+'sum' variable. This way, by the end of the loop, you have all values 
+added together 
+4.2. To calculate the average, divide the sum you calculated before by the 
+length of the array (because that's the number of elements)
+4.3. Call the function with the 'totals' array
+*/
+//anther way to write the needed function 
+/*
+let bills= [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips=[] ;
+let totals =[];
+function calcTip(bills){
+    for(let i =0 ; i< bills.length ; i++){
+        if (bills[i] >=50 && bills[i] <=300){
+            tips[i]=.15;
+            totals[i]= bills[i]+(bills[i]*tips[i]);
+            console.log(`the total value of bill(${bills[i]})=${totals[i]}`);
+            
+        }
+        else if (bills[i] >300){
+             tips[i]=.20;
+            totals[i]= bills[i]+(bills[i]*tips[i]);
+            console.log(`the total value of bill(${bills[i]})=${totals[i]}`);
+        }
+        else{
+            console.log(`bill must be more than 50 your current bill is (${bills[i]})`);
+            tips[i]=0;
+            totals[i]= bills[i];
+        }
+    }
+    
+    return  totals;
+}
+
+totals2=calcTip(bills);
+ console.log("total values =" +totals2);
+ */
+
+ calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  }
+  
+  /* Write your code below. Good luck! 🙂 */
+  let bills= [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+  let tips=[] ;
+  let totals =[];
+  for(let i =0 ; i< bills.length ; i++){
+      tips[i]=calcTip(bills[i])
+      totals[i]= bills[i]+tips[i];
+      console.log(`the total value of bill(${bills[i]})=${totals[i]}`);
+          
+      }
+  
+  console.log("total values =" +totals);
+  
+  function calcAverage2(arr){
+      let totalAverage=0;
+      for (let i =0 ; i< arr.length ; i++){
+      totalAverage += arr[i];
+          
+      }
+      console.log(totalAverage);
+      totalAverage= totalAverage/arr.length ;
+      console.log(totalAverage);
+      return totalAverage;
+  }
+      let result = calcAverage2(bills);
+  console.log(result);
